77e768d9adf82ed765fb8f4be8274c66d83d5805
Sala D3 <CSalaD3@uniboyaca.edu.co>
2024-05-25 10:49:32 -0500

    factura

--- views/FacturaCliente.php
+++ views/FacturaCliente.php
@@ -1,5 +1,3 @@
-
-
 <?php
 require_once '../controllers/ProductoController.php';
 require_once '../controllers/database.php';
@@ -20,39 +18,72 @@ $productos = $productoController->obtenerProductos();
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SPORT SHOP - Factura</title>
     <link rel="stylesheet" href="../styles.css">
 </head>
+
 <body>
     <h2>Productos Disponibles</h2>
-    <h3>Seleccione los productos que va a llevar</h3> 
-    <table>
-        <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nombre</th>
-                <th>Precio</th>
-                <th>Seleccionar</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($productos as $producto): ?>
-            <tr>
-                <td><?php echo $producto['id']; ?></td>
-                <td><?php echo $producto['nombre']; ?></td>
-                <td><?php echo $producto['precio']; ?></td>
-                <td>
-                    <button onclick="mostrarDetalles(<?php echo $producto['id']; ?>)">Seleccionar</button>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
+    <h3>Seleccione los productos que va a llevar</h3>
+    <form action="FacturaCliente.php" method="post">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($productos as $producto) : ?>
+                    <tr>
+                        <td><?php echo $producto['id']; ?></td>
+                        <td><?php echo $producto['nombre']; ?></td>
+                        <td><?php echo $producto['precio']; ?></td>
+                        <td>
+                            <input type="number" name="productos<?php echo $producto['id']; ?>" value="0">
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <button id="guardarSeleccion">Guardar Selección</button>
+    </form>
 
-    
-  
+
+    <form action="RegistrarCliente.php" method="post">
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Precio</th>
+                    <th>Cantidad</th>
+
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php foreach ($productos as $producto) : ?>
+                    <tr>
+                        <?php if (!empty($_POST['productos' . $producto['id'] ])) : ?>
+                            <td> <?php echo $producto['nombre']; ?></td>
+                            <td> <?php echo  $producto['precio']; ?></td>
+                            <td> <?php echo  $_POST['productos' . $producto['id'] ]; ?></td>
+                        <?php endif; ?>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <button id="GenerarFactura">Generar Factura</button>
+    </form>
+
+
 </body>
+
 </html>
---
+++ views/RegistrarCliente.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DATOS FACTURACION</title>
+    <link rel="stylesheet" href="../styles.css">
+
+</head>
+
+<body>
+    <h2>Datos del Cliente</h2>
+    <form action="procesar_cliente.php" method="post">
+        <label for="nombre">Nombre Completo</label> <br>
+        <input type="text" id="nombre" name="nombre" required><br>
+
+        <label for="tipoDocumento">Tipo de Documento</label><br>
+        <input list="TipDocument" placeholder="Elige un tipo de documento:" id="tipoD" name="tipoDoc" required><br>
+
+        <datalist id="TipDocument">
+            <option value="CC">CC</option>
+            <option value="CE">CE</option>
+            <option value="NIT">NIT</option>
+            <option value="TI">TI</option>
+            <option value="Otro">Otro</option>
+
+
+
+        </datalist>
+        <label for="email">Numero de Domento</label> <br>
+        <input type="numero" id="Numdocumeto" name="NumDoc"><br>
+
+        <label for="email">email</label> <br>
+        <input type="email" id="email" name="email"><br>
+
+        <label for="telefono">Teléfono</label> <br>
+        <input type="text" id="telefono" name="telefono" required><br>
+
+       
+
+        <button type="submit">Enviar</button>
+    </form>
+
+</body>
+
+</html>
